55026646cac6b44257b6ae48117bfcdd4ebb3b27
aryana-chitnis <ac1000@pleasantonusd.net>
2025-01-10 22:47:30 +0000

    added a mian static method, fixed other bugs

--- probone.java
+++ probone.java
@@ -1,12 +1,14 @@
 public class probone {
     
-    int y = 0;
+    public static void main (String[] args) {
+    int y = 0; 
     
     for (int x =0; x < 1000;x++ ) {
     if (x % 3 == 0 || x % 5 == 0) {
-    y =+ x;
+    y += x;
     }
 
     }
     System.out.println("The sum of all natural numbers below 1000 is " + y );
+} 
 }
