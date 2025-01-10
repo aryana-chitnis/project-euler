a49d95d5cd64e84d415ad2aa0a80a257ec58648f
aryana-chitnis <ac1000@pleasantonusd.net>
2025-01-10 22:39:13 +0000

    i made a simple code that uses a loop to run through 1000

---
+++ probone.java
@@ -0,0 +1,12 @@
+public class probone {
+    
+    int y = 0;
+    
+    for (int x =0; int x < 1000;x++ ) {
+    if (x % 3 == 0 || x % 5 == 0) {
+    y =+ x;
+    }
+
+    }
+    System.out.println("The sum of all natural numbers below 1000 is " + y );
+}
