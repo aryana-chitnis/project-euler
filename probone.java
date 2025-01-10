7f95aed79d52f5d4684d415ec164739ea4ea5a67
aryana-chitnis <ac1000@pleasantonusd.net>
2025-01-10 22:40:24 +0000

    fixed a bug in the for loop

--- probone.java
+++ probone.java
@@ -2,7 +2,7 @@ public class probone {
     
     int y = 0;
     
-    for (int x =0; int x < 1000;x++ ) {
+    for (int x =0; x < 1000;x++ ) {
     if (x % 3 == 0 || x % 5 == 0) {
     y =+ x;
     }
